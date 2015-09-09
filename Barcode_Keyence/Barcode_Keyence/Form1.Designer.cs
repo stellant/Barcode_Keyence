4ce4f370629338bedb58dfe60d1339d576eeae37
stellant <thangavel.ayyasamy@wipro.com>
2015-09-09 16:39:21 +0530

    First Commit

---
+++ Barcode_Keyence/Barcode_Keyence/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Barcode_Keyence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_dataport = new System.Windows.Forms.TextBox();
+            this.textBox_communicationport = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ipaddress4 = new System.Windows.Forms.TextBox();
+            this.textBox_ipaddress3 = new System.Windows.Forms.TextBox();
+            this.textBox_ipaddress2 = new System.Windows.Forms.TextBox();
+            this.textBox_ipaddress1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox_status = new System.Windows.Forms.TextBox();
+            this.barcodeReaderControl1 = new Keyence.AutoID.BarcodeReaderControl();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button_close = new System.Windows.Forms.Button();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_dataport);
+            this.groupBox2.Controls.Add(this.textBox_communicationport);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox_ipaddress4);
+            this.groupBox2.Controls.Add(this.textBox_ipaddress3);
+            this.groupBox2.Controls.Add(this.textBox_ipaddress2);
+            this.groupBox2.Controls.Add(this.textBox_ipaddress1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(412, 84);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBox_dataport
+            // 
+            this.textBox_dataport.Location = new System.Drawing.Point(320, 47);
+            this.textBox_dataport.Multiline = true;
+            this.textBox_dataport.Name = "textBox_dataport";
+            this.textBox_dataport.Size = new System.Drawing.Size(70, 20);
+            this.textBox_dataport.TabIndex = 16;
+            // 
+            // textBox_communicationport
+            // 
+            this.textBox_communicationport.Location = new System.Drawing.Point(228, 47);
+            this.textBox_communicationport.Multiline = true;
+            this.textBox_communicationport.Name = "textBox_communicationport";
+            this.textBox_communicationport.Size = new System.Drawing.Size(70, 20);
+            this.textBox_communicationport.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Enter Communication and Data Port";
+            // 
+            // textBox_ipaddress4
+            // 
+            this.textBox_ipaddress4.Location = new System.Drawing.Point(141, 47);
+            this.textBox_ipaddress4.Multiline = true;
+            this.textBox_ipaddress4.Name = "textBox_ipaddress4";
+            this.textBox_ipaddress4.Size = new System.Drawing.Size(37, 20);
+            this.textBox_ipaddress4.TabIndex = 13;
+            // 
+            // textBox_ipaddress3
+            // 
+            this.textBox_ipaddress3.Location = new System.Drawing.Point(98, 47);
+            this.textBox_ipaddress3.Multiline = true;
+            this.textBox_ipaddress3.Name = "textBox_ipaddress3";
+            this.textBox_ipaddress3.Size = new System.Drawing.Size(37, 20);
+            this.textBox_ipaddress3.TabIndex = 12;
+            // 
+            // textBox_ipaddress2
+            // 
+            this.textBox_ipaddress2.Location = new System.Drawing.Point(55, 47);
+            this.textBox_ipaddress2.Multiline = true;
+            this.textBox_ipaddress2.Name = "textBox_ipaddress2";
+            this.textBox_ipaddress2.Size = new System.Drawing.Size(37, 20);
+            this.textBox_ipaddress2.TabIndex = 11;
+            // 
+            // textBox_ipaddress1
+            // 
+            this.textBox_ipaddress1.Location = new System.Drawing.Point(12, 47);
+            this.textBox_ipaddress1.Multiline = true;
+            this.textBox_ipaddress1.Name = "textBox_ipaddress1";
+            this.textBox_ipaddress1.Size = new System.Drawing.Size(37, 20);
+            this.textBox_ipaddress1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Enter IP Address";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox_status);
+            this.groupBox4.Location = new System.Drawing.Point(13, 92);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(412, 106);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            // 
+            // textBox_status
+            // 
+            this.textBox_status.Location = new System.Drawing.Point(8, 16);
+            this.textBox_status.Multiline = true;
+            this.textBox_status.Name = "textBox_status";
+            this.textBox_status.Size = new System.Drawing.Size(398, 81);
+            this.textBox_status.TabIndex = 0;
+            // 
+            // barcodeReaderControl1
+            // 
+            this.barcodeReaderControl1.BackColor = System.Drawing.Color.Black;
+            this.barcodeReaderControl1.Location = new System.Drawing.Point(8, 14);
+            this.barcodeReaderControl1.Name = "barcodeReaderControl1";
+            this.barcodeReaderControl1.Size = new System.Drawing.Size(100, 64);
+            this.barcodeReaderControl1.TabIndex = 12;
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(10, 58);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(96, 40);
+            this.button_close.TabIndex = 14;
+            this.button_close.Text = "Close";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(10, 14);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(96, 40);
+            this.button_connect.TabIndex = 13;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_connect);
+            this.groupBox1.Controls.Add(this.button_close);
+            this.groupBox1.Location = new System.Drawing.Point(431, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 106);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.barcodeReaderControl1);
+            this.groupBox3.Location = new System.Drawing.Point(431, 5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(116, 84);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 211);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Barcode Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_dataport;
+        private System.Windows.Forms.TextBox textBox_communicationport;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ipaddress4;
+        private System.Windows.Forms.TextBox textBox_ipaddress3;
+        private System.Windows.Forms.TextBox textBox_ipaddress2;
+        private System.Windows.Forms.TextBox textBox_ipaddress1;
+        private System.Windows.Forms.Label label1;
+        private Keyence.AutoID.BarcodeReaderControl barcodeReaderControl1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox_status;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+
+    }
+}
+
---
+++ Barcode_Keyence/Barcode_Keyence/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Keyence.AutoID;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace Barcode_Keyence
+{
+    public partial class Form1 : Form
+    {
+        //Integer variable to store command port
+        private int commandport;
+        //Integer variable to store data port
+        private int dataport;
+        //StreamWriter variables to write data into file and logs into log file
+        private StreamWriter streamWriter,streamLogWriter;
+        //Data object to hold the barcode data
+        private BarcodeData barcodeData;
+        //Holds the path where the data file should be
+        private string filePath;
+        /// <summary>
+        /// A default constructor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Method to create a StreamWriter object to write barcode data
+        /// </summary>
+        /// <param name="filePath">Location of the file</param>
+        /// <returns></returns>
+        private StreamWriter getWriter(string filePath)
+        {
+            if (streamWriter == null)
+            {
+                streamWriter = new StreamWriter(filePath,true);
+            }
+            return streamWriter;
+        }
+        /// <summary>
+        /// Method to create a StreamWriter object to write log data
+        /// </summary>
+        /// <param name="filePath">Location of the file</param>
+        /// <returns></returns>
+        private StreamWriter getLogWriter(string filePath)
+        {
+            if (streamLogWriter == null)
+            {
+                streamLogWriter = new StreamWriter(filePath, true);
+            }
+            return streamLogWriter;
+        }
+        /// <summary>
+        /// Method to write logs into log file
+        /// </summary>
+        /// <param name="status">Log Details</param>
+        private void WriteLog(string status)
+        {
+            getLogWriter("log.log").WriteLine(status);
+            getLogWriter("log.log").Flush();
+        }
+        /// <summary>
+        /// Method to write data into data file
+        /// </summary>
+        /// <param name="data">Barcode data details</param>
+        private void WriteData(string data)
+        {
+            getWriter(filePath).WriteLine(data);
+            getWriter(filePath).Flush();
+        }
+        private void OnFocusCommunicationPort(object Sender,EventArgs e)
+        {
+            textBox_communicationport.Text = "";
+        }
+        private void OnLostFocusCommunicationPort(object Sender, EventArgs e)
+        {
+            if (textBox_communicationport.Text.Equals(""))
+            {
+                textBox_communicationport.Text = "9003";
+            }
+        }
+        private void OnFocusDataPort(object Sender, EventArgs e)
+        {
+            textBox_dataport.Text = "";
+        }
+        private void OnLostFocusDataPort(object Sender, EventArgs e)
+        {
+            if (textBox_dataport.Text.Equals(""))
+            {
+                textBox_dataport.Text = "9004";
+            }
+        }
+        private void OnFocusIPAddress1(object Sender, EventArgs e)
+        {
+            textBox_ipaddress1.Text = "";
+        }
+        private void OnLostFocusIPAddress1(object Sender, EventArgs e)
+        {
+            if (textBox_ipaddress1.Text.Equals(""))
+            {
+                textBox_ipaddress1.Text = "192";
+            }
+            else if (Convert.ToInt32(textBox_ipaddress1.Text) > 255)
+            {
+                textBox_ipaddress1.Text = "192";
+            }
+        }
+        private void OnFocusIPAddress2(object Sender, EventArgs e)
+        {
+            textBox_ipaddress2.Text = "";
+        }
+        private void OnLostFocusIPAddress2(object Sender, EventArgs e)
+        {
+            if (textBox_ipaddress2.Text.Equals(""))
+            {
+                textBox_ipaddress2.Text = "168";
+            }
+            else if (Convert.ToInt32(textBox_ipaddress2.Text) > 255)
+            {
+                textBox_ipaddress2.Text = "168";
+            }
+        }
+        private void OnFocusIPAddress3(object Sender, EventArgs e)
+        {
+            textBox_ipaddress3.Text = "";
+        }
+        private void OnLostFocusIPAddress3(object Sender, EventArgs e)
+        {
+            if (textBox_ipaddress3.Text.Equals(""))
+            {
+                textBox_ipaddress3.Text = "1";
+            }
+            else if (Convert.ToInt32(textBox_ipaddress3.Text) > 255)
+            {
+                textBox_ipaddress3.Text = "1";
+            }
+        }
+        private void OnFocusIPAddress4(object Sender, EventArgs e)
+        {
+            textBox_ipaddress4.Text = "";
+        }
+        private void OnLostFocusIPAddress4(object Sender, EventArgs e)
+        {
+            if (textBox_ipaddress4.Text.Equals(""))
+            {
+                textBox_ipaddress4.Text = "1";
+            }
+            else if(Convert.ToInt32(textBox_ipaddress4.Text)>255)
+            {
+                textBox_ipaddress4.Text = "1";
+            }
+        }
+        /// <summary>
+        /// This is the method to initialize controls when form loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_ipaddress1.Text = "192";
+            textBox_ipaddress2.Text = "168";
+            textBox_ipaddress3.Text = "1";
+            textBox_ipaddress4.Text = "1";
+            textBox_communicationport.Text = "9003";
+            textBox_dataport.Text = "9004";
+            textBox_communicationport.GotFocus += OnFocusCommunicationPort;
+            textBox_communicationport.LostFocus += OnLostFocusCommunicationPort;
+            textBox_dataport.GotFocus += OnFocusDataPort;
+            textBox_dataport.LostFocus += OnLostFocusDataPort;
+            textBox_ipaddress1.GotFocus += OnFocusIPAddress1;
+            textBox_ipaddress1.LostFocus += OnLostFocusIPAddress1;
+            textBox_ipaddress2.GotFocus += OnFocusIPAddress2;
+            textBox_ipaddress2.LostFocus += OnLostFocusIPAddress2;
+            textBox_ipaddress3.GotFocus += OnFocusIPAddress3;
+            textBox_ipaddress3.LostFocus += OnLostFocusIPAddress3;
+            textBox_ipaddress4.GotFocus += OnFocusIPAddress4;
+            textBox_ipaddress4.LostFocus += OnLostFocusIPAddress4;
+        }
+        public TextBox getTextBoxStatus()
+        {
+            return textBox_status;
+        }
+        /// <summary>
+        /// This is the method to handle close button click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt";
+            if (saveFileDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                filePath = saveFileDialog1.FileName;
+                WriteLog("Data will be written to "+filePath+"     \n");
+                textBox_status.Text = "Data will be written to "+filePath+"     \n";
+                try
+                {
+                    Byte[] ipaddress = new byte[4];
+                    ipaddress[0] = Convert.ToByte(textBox_ipaddress1.Text.Trim());
+                    ipaddress[1] = Convert.ToByte(textBox_ipaddress2.Text.Trim());
+                    ipaddress[2] = Convert.ToByte(textBox_ipaddress3.Text.Trim());
+                    ipaddress[3] = Convert.ToByte(textBox_ipaddress4.Text.Trim());
+                    commandport = Convert.ToInt32(textBox_communicationport.Text.Trim());
+                    dataport = Convert.ToInt32(textBox_dataport.Text.Trim());
+                    IPAddress ip = new IPAddress(ipaddress);
+                    this.barcodeReaderControl1.IpAddress = ip.ToString();
+                    WriteLog("IP Address is " + ip.ToString() + "\n");
+                    textBox_status.Text = "IP Address is " + ip.ToString() + "\n";
+                    this.barcodeReaderControl1.Ether.CommandPort = commandport;
+                    WriteLog("Command Port is " + commandport.ToString() + "\n");
+                    textBox_status.Text = "Command Port is " + commandport.ToString() + "\n";
+                    this.barcodeReaderControl1.Ether.DataPort = dataport;
+                    WriteLog("Data Port is " + dataport.ToString() + "\n");
+                    textBox_status.Text = "Data Port is " + dataport.ToString() + "\n";
+                    this.barcodeReaderControl1.Comm.Interface = Interface.Ethernet;
+                    WriteLog("Interface is " + Interface.Ethernet.ToString() + "\n");
+                    textBox_status.Text = "Interface is " + Interface.Ethernet.ToString() + "\n";
+                    this.barcodeReaderControl1.ReaderType = ReaderType.SR_1000;
+                    WriteLog("Reader Type is " + ReaderType.SR_1000.ToString() + "\n");
+                    textBox_status.Text = "Reader Type is " + ReaderType.SR_1000.ToString() + "\n";
+                    textBox_status.AppendText("Socket Connecting...\n");
+                    this.barcodeReaderControl1.Connect();
+                    textBox_status.AppendText("Socket Connected...\n");
+                    this.barcodeReaderControl1.OnDataReceived += dataReceived;
+                    textBox_status.AppendText("Live View Starting...\n");
+                    this.barcodeReaderControl1.StartLiveView();
+                    textBox_status.AppendText("Live View Started...\n");
+                }
+                catch (SocketException ex)
+                {
+                    WriteLog(ex.StackTrace + Environment.NewLine + "   at " + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss:fff"));
+                    textBox_status.AppendText("Socket Exception Occured. Please refer log file." + "\n");
+                }
+                catch (Exception ex)
+                {
+                    WriteLog(ex.StackTrace + Environment.NewLine + "   at " + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss:fff"));
+                    textBox_status.AppendText("Exception Occured. Please refer log file." + "\n");
+                }
+                finally
+                {
+                    getLogWriter("log.log").Close();
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+        /// <summary>
+        /// This is the function to write barcode data into file and it is called whenever there is an OnDataReceived event occurs.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataReceived(object sender,OnDataReceivedEventArgs e)
+        {
+            try
+            {
+                barcodeData = new BarcodeData();
+                barcodeData.DataBytes = e.data;
+                WriteData(barcodeData.ToString() + "   " + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss:fff"));
+                textBox_status.AppendText(barcodeData.ToString());
+            }
+            catch (Exception ex)
+            {
+                textBox_status.AppendText(ex.ToString() + "\n");
+            }
+            finally
+            {
+                getWriter(filePath).Close();
+            }
+        }
+        /// <summary>
+        /// This is the method to handle close button click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.barcodeReaderControl1.OnDataReceived -= dataReceived;
+                this.barcodeReaderControl1.StopLiveView();
+                this.barcodeReaderControl1.Disconnect();
+                textBox_status.AppendText("Socked Closed...\n");
+            }
+            catch (Exception ex)
+            {
+                textBox_status.AppendText(ex.ToString() + "\n");
+            }
+        }
+
+        
+        
+    }
+}
